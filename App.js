37d1364bed9a6ce61ae7d4f57687853e3bb18ab2
shreevidyamatabhuvanesvari-gif <shreevidyamatabhuvanesvari@gmail.com>
2026-02-04 21:46:38 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * App.js
+ *
+ * Root application container.
+ * This file confirms that index.html and index.js
+ * are correctly wired together.
+ */
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#000",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#FF9933",
+        fontSize: "24px",
+        fontWeight: "600",
+      }}
+    >
+      धर्म ज्ञान क्विज<br />लोड हो रहा है…
+    </div>
+  );
+        }
