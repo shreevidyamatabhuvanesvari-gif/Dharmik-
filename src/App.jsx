6a3f1ab38794a1f56004f047d2be3fc289d91a84
shreevidyamatabhuvanesvari-gif <shreevidyamatabhuvanesvari@gmail.com>
2026-02-04 22:07:31 +0530

    Update and rename App.js to src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ background: "white", padding: 40 }}>
+      <h1>APP RENDER HO RAHI HAI</h1>
+      <p>React + Vite correctly working</p>
+    </div>
+  );
+}
